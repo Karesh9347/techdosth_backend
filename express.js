0a20746294ce2bb10a154fee636f3a4d649498a2
pattapu naresh <161163501+Karesh9347@users.noreply.github.com>
2024-09-21 21:01:07 +0530

    Update express.js

--- express.js
+++ express.js
@@ -5,10 +5,10 @@ const dotenv = require("dotenv");
 const Questions = require("./schema");
 const Coder = require("./usermodel"); 
 const Contest=require("./contestSchema")
-dotenv.config();
+
 
 const app = express();
-app.use(cors());
+
 app.use(express.json({ limit: '10mb' })); 
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
